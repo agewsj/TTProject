2871022e209560ccb3b9c98da8c716d9fbdc0814
agewsj <agewsj@gmail.com>
2020-04-09 20:46:50 +0900

    DoTween 추가 및 이미지 meta파일 추가

---
+++ TTProject/Assets/TTProject/Scripts/UI/PlayerUI/Mercenery.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Mercenery : MonoBehaviour
+{
+    public void PingpongAni()
+    {
+        
+    }
+}
